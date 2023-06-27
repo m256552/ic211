07c2159bad5fdf21e82b846dbf0ba895d02c79b9
Justin <git@justindowns.com>
2023-06-27 16:16:25 -0400

    added some pointers for part 4 lab 2

--- lab/Lab02/MidLibs.java
+++ lab/Lab02/MidLibs.java
@@ -14,12 +14,28 @@ public class MidLibs {
         cols = Integer.parseInt(args[1]);
         String [] A;
         A = WordRead.get(filename);
-        //new code here where we put the
-        //
-        //
-        //
-        //
+
+        /////// Start Part 4 //////////
+
+        // This is how to get a random noun.
+        String[] nouns = WordRead.get("nouns.txt");
+        Random rand = new Random(890);
+        String randomNoun = rand.nextInt(nouns.length);
+
+        // This is how to make a noun plural
+        String singleNoun = "dog";
+        String pluralNoun = singleNoun + "s";
+
+
+
+        // for every word in A:
+        //   if A[i].equals("@nonp"):
+        //      A[i] = random plural noun
+
+        //////// End Part 4 ///////////
+
         Formatter.writeInColumns(A, cols);        
  
     }
+
 }
