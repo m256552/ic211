8f5a2010ada1a31664c9793fb960563187e9a402
Justin <git@justindowns.com>
2023-06-27 14:39:04 -0400

    Added Midlibs skeleton for Lab 2

---
+++ lab/Lab02/MidLibs.java
@@ -0,0 +1,12 @@
+public class MidLibs {
+
+
+    public static void main(String[] args) {
+
+        // Try running the command
+        // java MidLibs hello everyone everwhere
+        System.out.print("The first argument is " + args[0] + "\n" );
+        System.out.print("The second argument is " + args[1] + "\n" );
+        System.out.print("The third argument is " + args[2] + "\n" );
+    }
+}
